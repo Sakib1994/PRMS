ae990bbd65149826a4f4d93f561ffd075d36342f
mdnazmussakibcse <mdnazmussakibcse@outlook.com>
2019-04-05 14:30:02 +0600

    Initial

--- Controllers/EnrollmentController.cs
+++ Controllers/EnrollmentController.cs
@@ -1,6 +1,5 @@
 ﻿using OfficeOpenXml;
 using PRMS.Models;
-using PRMS.Models.CSE;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -15,46 +14,36 @@ namespace PRMS.Controllers
 {
     public class EnrollmentController : Controller
     {
-        private CseDbContext csedb = new CseDbContext();
+
         private ProjectDB db = new ProjectDB();
         //
         // GET: /Enrollment/
         public ActionResult Index()
-        {
-
+        {   if (!HasSession()) return RedirectToAction("Index", "Home");
             ViewBag.faculties = db.Faculty.ToList();
             return View();
+        }
+
+        protected Boolean HasSession()
+        {   Admin admin = HttpContext.Session[Variables.AdminSession] as Admin;
+            if (admin == null)
+            {
+                return false;
+            }
+           return true;
         }
 
         [HttpPost]
         public ActionResult Index(String faculty, int semester)
-        {
-
+        {   if (!HasSession()) return RedirectToAction("Index", "Home");
             String fileName = faculty + "_Enroll_" + semester + ".xlsx";
-
             Server.MapPath("~/App_Data/CSE/" + fileName);
-
             return RedirectToAction("UploadEnrollment", "Enrollment", new { faculty = faculty, semester = semester });
         }
 
-
-
-        public ActionResult Download(String faculty, int semester)
-        {
-
-
-
-            ViewBag.faculties = db.Faculty.ToList();
-            return View();
-        }
-
-
-        public ActionResult UploadEnrollment(String faculty, int semester)
-        {
-
+       public ActionResult UploadEnrollment(String faculty, int semester)
+        {   if (!HasSession()) return RedirectToAction("Index", "Home");
             String sem = SemesterName(semester);
-
-
             ViewBag.semester = semester;
             ViewBag.faculty = faculty;
             return View();
@@ -64,6 +53,10 @@ namespace PRMS.Controllers
         [HttpPost]
         public ActionResult UploadEnrollment(String faculty, int semester, HttpPostedFileBase postedFile)
         {
+            if (!HasSession()) return RedirectToAction("Index", "Home");
+
+            ViewBag.semester = semester;
+            ViewBag.faculty = faculty;
             String fileName;
             if (faculty != null && postedFile != null)
             {
@@ -74,9 +67,7 @@ namespace PRMS.Controllers
                     fileName = Path.GetFileName(postedFile.FileName);
 
                     if (fileName.ToLower().Contains(".xls") || fileName.ToLower().Contains(".xlsx"))
-                    {
-
-                        byte[] fileBytes = new byte[postedFile.ContentLength];
+                    {     byte[] fileBytes = new byte[postedFile.ContentLength];
 
                         var data = postedFile.InputStream.Read(fileBytes, 0, Convert.ToInt32(postedFile.ContentLength));
                         using (var package = new ExcelPackage(postedFile.InputStream))
@@ -85,64 +76,57 @@ namespace PRMS.Controllers
                             var workSheet = currentSheet.First();
                             var noOfCol = workSheet.Dimension.End.Column;
                             var noOfRow = workSheet.Dimension.End.Row;
-
                             String property;
-                            Boolean ck=false;
+                            Boolean ck = false;
 
-                            for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                            CRUD crd = new CRUD(faculty);
+                            crd.TruncateCurrentSemester(semester-1);
+
+
+                             for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
                             {
                                 IDictionary<string, object> dict = new Dictionary<string, object>();
-                          
-
-                                dynamic enrollment = new DynamicDictionary();
-                            //    List<KeyValuePair<string, object>> enroll = new List<KeyValuePair<string, object>>();
-
-                                for (int i = 1; i <= noOfCol; i++)
+                                try
                                 {
-                                      
-                                    property  = workSheet.Cells[1, i].Value.ToString();
+                                    for (int i = 1; i <= noOfCol; i++)
+                                    {
+                                        property = workSheet.Cells[1, i].Value.ToString();
 
-                                    if (i == 1 || i == 4)
-                                    {
-                                        dict.Add(new KeyValuePair<string, object>(property, workSheet.Cells[rowIterator, i].Value));  
-                                    }
-                                    else if (i == 2 || i == 3 || i == 5)
-                                    {
-                                         dict.Add(new KeyValuePair<string, object>(property,Convert.ToInt32( workSheet.Cells[rowIterator, i].Value)));  
-                                      
-                                    }
-                                    else
-                                    {
-                                          dict.Add(new KeyValuePair<string, object>(property, Convert.ToBoolean(workSheet.Cells[rowIterator, i].Value)));  
-                                      
-                                    }
-                                      
+                                        if (i == 1 || i == 4)
+                                        {
+                                            dict.Add(new KeyValuePair<string, object>(property, workSheet.Cells[rowIterator, i].Value));
+                                        }
+                                        else if (i == 2 || i == 3 || i == 5)
+                                        {   
+                                            dict.Add(new KeyValuePair<string, object>(property, Convert.ToInt32(workSheet.Cells[rowIterator, i].Value)));
+                                            }
+                                        else
+                                        {
+                                            if (workSheet.Cells[rowIterator, i].Value != null)
+                                                dict.Add(new KeyValuePair<string, object>(property, Convert.ToBoolean(workSheet.Cells[rowIterator, i].Value)));
+                                            else
+                                                dict.Add(new KeyValuePair<string, object>(property, false));
+                                        }
+                                     }
+
+                                    crd = new CRUD(faculty);
+                                    ck = crd.InsertEnrollment(dict);
+                                }
+                                catch (Exception ex)
+                                {
+                                    ViewBag.Message = Messasges.InvalidExcelData;
+                                    return View();
                                 }
 
-                                if (faculty.Equals("selected"))
+                                if (ck == false)
                                 {
-                                    ViewBag.Message = "Select A Faculty";
+                                    ViewBag.ck = ck;
+                                    return View();
                                 }
-                                else
-                                {
-                                    CRUD crd = new CRUD(faculty);
-                                   ck=  crd.InsertEnrollment(dict);
-
-                                  if (ck == false)
-                                  {
-                                      ViewBag.ck = ck;
-                                      break;
-                                  }
-                                 
-                                }
-
-                           }
-
-                            ViewBag.ck = ck;
-
-                        }
-
-                    }
+                            }
+                           ViewBag.ck = ck;
+                         }
+                      }
                     else
                     {
                         //format not supported
@@ -157,14 +141,15 @@ namespace PRMS.Controllers
             }
 
 
-            ViewBag.semester = semester;
-            ViewBag.faculty = faculty;
+
             return View();
         }
 
 
         public ActionResult ManageEnrollment(string msg)
         {
+            if (!HasSession()) return RedirectToAction("Index", "Home");
+
             ViewBag.msg = msg;
             ViewBag.faculties = db.Faculty.ToList();
             return View();
@@ -173,14 +158,12 @@ namespace PRMS.Controllers
         [HttpPost]
         public ActionResult ManageEnrollment(string faculty, int semester, int id)
         {
+            if (!HasSession()) return RedirectToAction("Index", "Home");
 
+            IDictionary<string, object> dict = new Dictionary<string, object>();
 
-               IDictionary<string, object> dict = new Dictionary<string, object>();
-
-            CRUD crud=new CRUD(faculty);
+            CRUD crud = new CRUD(faculty);
             dict = crud.SelectEnrollment(semester, id);
-
-             
 
             if (dict == null)
             {
@@ -198,34 +181,31 @@ namespace PRMS.Controllers
             }
         }
 
-
-
         [HttpPost]
         public ActionResult EditEnrollment(String faculty, String name, int studentid, int regno, int semester, String session, String[] subjectlist)
         {
-
-
+            if (!HasSession()) return RedirectToAction("Index", "Home");
 
             IDictionary<string, object> dict = new Dictionary<string, object>();
-             
+
             CRUD crud = new CRUD(faculty);
-       //     dict = 
+            //     dict = 
             dict = crud.SelectEnrollment(semester, studentid);
 
             List<string> keys = new List<string>(dict.Keys);
-            
+
             foreach (string item in keys)
             {
                 if (dict[item].Equals(true))
                 {
-                  dict[item] = false;
+                    dict[item] = false;
                 }
 
             }
-              
+
             foreach (String s in subjectlist)
             {
-                dict[s]= true; 
+                dict[s] = true;
             }
             crud.UpdateEnrollment(dict);
 
@@ -237,34 +217,35 @@ namespace PRMS.Controllers
             ViewBag.message = "Updated Successfully..!";
             return View();
         }
-       
+
         public ActionResult Delete(int id, int semester, String faculty)
         {
+            if (!HasSession()) return RedirectToAction("Index", "Home");
+
             IDictionary<string, object> dict = new Dictionary<string, object>();
 
             CRUD crud = new CRUD(faculty);
             dict = crud.SelectEnrollment(semester, id);
-           Boolean ck=  crud.DeleteEnrollment( id, semester);
+            Boolean ck = crud.DeleteEnrollment(id, semester);
 
-           if (ck == true)
-           {
-              string message = "Student Enrollment Delete Successfully..!";
-               return RedirectToAction("ManageEnrollment", "Enrollment", new  { msg =message });
+            if (ck == true)
+            {
+                string message = "Student Enrollment Delete Successfully..!";
+                return RedirectToAction("ManageEnrollment", "Enrollment", new { msg = message });
 
-           }
-           else
-           {
-               ViewBag.message = "Some Problem when Delete Student Enrollment..!";
-               ViewBag.en = dict;
-               ViewBag.faculty = faculty;
-               ViewBag.semester = semester;
-               return View("EditEnrollment");
+            }
+            else
+            {
+                ViewBag.message = "Some Problem when Delete Student Enrollment..!";
+                ViewBag.en = dict;
+                ViewBag.faculty = faculty;
+                ViewBag.semester = semester;
+                return View("EditEnrollment");
 
-           }
+            }
 
 
         }
-
 
 
         private static string SemesterName(int semester)
